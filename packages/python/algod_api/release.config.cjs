d12df8346a5c0d4b9adb89d5b1ad80d9e1fcd5cd
Neil Campbell <neil.campbell@makerx.com.au>
2025-05-10 00:13:07 +0800

    feat: package py algod api client

--- packages/python/algod_api/release.config.cjs
+++ packages/python/algod_api/release.config.cjs
@@ -1,10 +1,7 @@
 const releaseUtils = require("../../../utils/semantic-release.cjs");
 
-const config = releaseUtils.getConfig({
+module.exports = releaseUtils.getConfig({
   language: "python",
   package_name: "algod_api",
+  assets: ["../../../artifacts/algokit_algod_api*.whl"],
 });
-
-config.plugins = [...config.plugins, ["@semantic-release/npm", { npmPublish: false }]];
-
-module.exports = config;
--- utils/semantic-release.cjs
+++ utils/semantic-release.cjs
@@ -12,7 +12,9 @@ module.exports = {
     const { language, package_name } = opts;
     const assets = opts.assets || [];
     return {
-      branches: ["release", { name: "main", prerelease: "alpha" }],
+      branches: ["release", { name: "feat/package-api", prerelease: "alpha" }],
+      // TODO: NC - Revert this back after testing
+      // branches: ["release", { name: "main", prerelease: "alpha" }],
       repositoryUrl: "https://github.com/algorandfoundation/algokit-core",
       tagFormat: `${language}/${package_name}` + "@${version}",
       plugins: [
