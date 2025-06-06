ba89749519cb740fcb18e0739a79fc9960867dcf
Joe Polny <joepolny@gmail.com>
2025-06-06 08:37:38 -0400

    wip: blocking HTTPClient

--- crates/algokit_utils/src/lib.rs
+++ crates/algokit_utils/src/lib.rs
@@ -1,16 +1,53 @@
 use algokit_transact::AlgorandMsgpack;
 use algokit_transact::Transaction;
+use reqwest;
 
-#[derive(Debug)]
+pub trait HTTPClient {
+    // TODO: Make this async
+    fn json(&self, path: &str) -> Result<String, String>;
+}
+
+// TODO: Put reqwest and this default client behind a feature flag
+struct DefaultHTTPClient {
+    host: String,
+}
+
+impl DefaultHTTPClient {
+    pub fn new(host: &str) -> Self {
+        DefaultHTTPClient {
+            host: host.to_string(),
+        }
+    }
+}
+
+impl HTTPClient for DefaultHTTPClient {
+    fn json(&self, path: &str) -> Result<String, String> {
+        let response = reqwest::blocking::get(self.host.clone() + path)
+            .map_err(|e| e.to_string())?
+            .text()
+            .map_err(|e| e.to_string())?;
+
+        Ok(response)
+    }
+}
+
 pub struct Composer {
     transactions: Vec<Transaction>,
+    http_client: Box<dyn HTTPClient>,
 }
 
 impl Composer {
     pub fn new() -> Self {
         Composer {
             transactions: Vec::new(),
+            http_client: Box::new(DefaultHTTPClient::new(
+                "https://testnet-api.4160.nodely.dev",
+            )),
         }
+    }
+
+    pub fn set_http_client(&mut self, client: Box<dyn HTTPClient>) {
+        self.http_client = client;
     }
 
     pub fn add_transaction(&mut self, transaction: Transaction) -> Result<(), String> {
@@ -31,6 +68,12 @@ impl Composer {
 
     pub fn transactions(&self) -> Vec<Transaction> {
         self.transactions.clone()
+    }
+
+    pub fn get_suggested_params(&self) -> Result<String, String> {
+        let path = "/v2/transactions/params";
+
+        Ok(self.http_client.json(path)?)
     }
 }
 
@@ -67,5 +110,12 @@ mod tests {
         let encoded = composer.encode();
         assert!(encoded.is_ok());
         assert_eq!(encoded.unwrap().len(), 5);
+    }
+
+    #[test]
+    fn test_get_suggested_params() {
+        let composer = Composer::new();
+        let response = composer.get_suggested_params().unwrap();
+        println!("Suggested Params: {}", response);
     }
 }
